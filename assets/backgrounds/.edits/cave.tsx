a5e2c34c414990dd010feb6525899391e42c405a
Jaime Hernandez <jaimevazz@gmail.com>
2026-06-01 20:41:37 -0600

    Add cave tileset and fix tiled project

--- assets/backgrounds/.edits/cave.tsx
+++ assets/backgrounds/.edits/cave.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="Cave" tilewidth="16" tileheight="16" tilecount="264" columns="11">
- <image source="../../../../../Downloads/cave.png" width="176" height="384"/>
+ <image source="cave.png" width="176" height="384"/>
 </tileset>
