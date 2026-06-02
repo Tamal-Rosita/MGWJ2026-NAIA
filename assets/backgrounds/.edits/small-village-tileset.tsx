f0d91ae4f9a514b4d70bc0bfe18df0cdc9e745ff
Jaime Hernandez <jaimevazz@gmail.com>
2026-06-01 19:54:36 -0600

    Update scenes with new backgrounds from Tiled. Add edit files

---
+++ assets/backgrounds/.edits/small-village-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="small-village-tileset" tilewidth="8" tileheight="8" tilecount="182" columns="14" fillmode="preserve-aspect-fit">
+ <image source="small-village-tileset.png" width="112" height="104"/>
+</tileset>
