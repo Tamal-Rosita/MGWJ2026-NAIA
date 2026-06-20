2591d21d97ab70a4531f195db6313712e0d968f7
SebPorthoz <134549155+SebPorthoz@users.noreply.github.com>
2026-06-20 09:31:07 -0600

    Agrego Prólogo

---
+++ assets/.edits/store.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="store" tilewidth="8" tileheight="8" tilecount="390" columns="13">
+ <image source="../backgrounds/store.png" width="104" height="240"/>
+</tileset>
