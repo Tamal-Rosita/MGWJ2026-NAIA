6b415612eac2ac3e1a05e9dd1c84be682e7ce42d
SebPorthoz <134549155+SebPorthoz@users.noreply.github.com>
2026-07-03 15:48:33 -0600

    Mejora de los caves dentro del dungeon,más primeras versiones de las cinemáticas. Estructura principal del flujo del juego terminada

---
+++ assets/.edits/houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="houses" tilewidth="8" tileheight="8" tilecount="50" columns="5">
+ <image source="../tilesets/houses.png" width="44" height="80"/>
+</tileset>
---
+++ assets/.edits/skulls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="skulls" tilewidth="8" tileheight="8" tilecount="2279" columns="43">
+ <image source="../tilesets/skulls.png" width="347" height="427"/>
+</tileset>
