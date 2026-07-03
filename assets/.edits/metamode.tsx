f0d6f56ee5fff8c3127fbdc4842575977d79bcbb
SebPorthoz <134549155+SebPorthoz@users.noreply.github.com>
2026-07-02 20:22:53 -0600

    Agregué la escena Dark Cave con el laberinto invisible

--- assets/.edits/metamode.tsx
+++ assets/.edits/metamode.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="metamode" tilewidth="8" tileheight="8" tilecount="3680" columns="40">
- <image source="../../../../../../../Volumes/Extreme SSD/1 Descargas/Metamode pt 1.png" width="320" height="736"/>
+ <image source="../tilesets/metamode.png" width="320" height="736"/>
 </tileset>
